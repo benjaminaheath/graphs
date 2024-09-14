6ba15372e2eb2c346138691b3c25d3676ab796f6
Ben Heath <benjaheath@gmail.com>
2024-09-14 11:01:13 +0100

    Merge directed and undirected graph classes

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import random
 import itertools
 
-class UndirectedGraph:
+class Graph:
     def __init__(self, nodes: int):
         self._v = [set() for _ in range(nodes)]
         self.numVertices = nodes
@@ -18,13 +18,22 @@ class UndirectedGraph:
         self._v[v].add(u)
         self.numEdges +=1
 
+    def addDiEdge(self, u: int, v: int) -> None:
+        self._v[u].add(v)
+        self.numEdges +=1
+
+    def delDiEdge(self, u: int, v: int) -> None:
+        if v in self._v[u]: 
+            self._v[u].remove(v)
+            self.numEdges -=1
+
     def delEdge(self, u: int, v: int) -> None:
         if v in self._v[u] and u in self._v[v]: 
             self._v[u].remove(v)
             self._v[v].remove(u)
             self.numEdges -=1
 
-    def randomGraph(self) -> None:
+    def randomUndirectedGraph(self) -> None:
         # vertex set
         vset = list([i for i in range(0,self.numVertices)])
         
@@ -52,28 +61,7 @@ class UndirectedGraph:
             
             eset.remove(new_edge)
 
-class DirectedGraph:
-    def __init__(self, nodes: int):
-        self._v = [set() for _ in range(nodes)]
-        self.numVertices = nodes
-        self.numEdges = 0
-    
-    def __repr__(self):
-        graphstr = str()
-        for v in range(len(self._v)):
-            graphstr +=  "v["+str(v)+"] = "+str(self._v[v])+"\n"
-        return graphstr
-    
-    def addEdge(self, u: int, v: int) -> None:
-        self._v[u].add(v)
-        self.numEdges +=1
-
-    def delEdge(self, u: int, v: int) -> None:
-        if v in self._v[u]: 
-            self._v[u].remove(v)
-            self.numEdges -=1
-
-    def randomGraph(self) -> None:
+    def randomDirectedGraph(self) -> None:
         # vertex set
         vset = list([i for i in range(0,self.numVertices)])
         
@@ -97,13 +85,18 @@ class DirectedGraph:
             new_edge = edges[index_edge]
             u, v = tuple(new_edge)
             
-            self.addEdge(u,v) 
+            self.addDiEdge(u,v) 
             
             eset.remove(new_edge)
 
+
 if __name__=="__main__":
-    for _ in range(5): 
-        g = DirectedGraph(6)
-        g.randomGraph()
-        print(g)
+    di = Graph(6)
+    di.randomDirectedGraph()
+
+    un = Graph(4)
+    un.randomUndirectedGraph()
+    print(di)
+    print(un)
+
 
